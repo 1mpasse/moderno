9a1aa12e99a13e73d3e5205f927fe2142fe4cf0b
1mpasse <dimasy1231@gmail.com>
2021-01-22 13:58:24 +0200

    added page new-items

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function(){
-
+    //диапазон цен
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+    });
+  
+    $('.icon-th-list').on('click', function(){
+      $('.product__item').addClass('list');
+      $('.icon-th-list').addClass('active');
+      $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function(){
+      $('.product__item').removeClass('list');
+      $('.icon-th-large').addClass('active');
+      $('.icon-th-list').removeClass('active');
+    });
 
     $(".rate-star").rateYo({
         rating: 5,
